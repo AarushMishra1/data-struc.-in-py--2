4417c0e894c7c3f51da086fa4157db4295f896d0
AarushMishra1 <a.mishra123@hotmail.com>
2025-05-14 21:47:11 +0530

    third commit

--- main.py
+++ main.py
@@ -24,3 +24,31 @@ print(my_tuple[1:4])
 #interating through a tuple
 for letter in my_tuple:
     print("hello", letter)
+
+print("-------SET-------")
+
+
+
+#sets
+my_set = {1, 2, 2, 3, 4, 4} #duplicates not supported
+print(set(my_set))
+my_set.add(5)
+print("Update set",my_set)
+
+set1 = my_set
+set2 = {2, 5, 3, 7}
+
+print("\nSet1: ", set1)
+print("\nSet2: ", set2)
+
+print("Difference")
+print(set1.difference(set2))
+print ("Symmetric Difference")
+print(set1.symmetric_difference(set2))
+
+
+
+
+
+
+
